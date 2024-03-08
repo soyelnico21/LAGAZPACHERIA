5b9bfd32be8d52f5b781e74071833b31e9495977
Nicolas Rodriguez <nicolase.rodrigueza99@gmail.com>
2024-03-08 16:47:54 +1100

    slider done

--- main.js
+++ main.js
@@ -9,3 +9,46 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+const btnleft = document.querySelector(".btn-left");
+const btnright = document.querySelector(".btn-right");
+const sliders = document.querySelector(".sliders");
+const images = document.querySelector(".slider-section");
+
+btnleft.addEventListener("click", e => movetoleft() )
+btnright.addEventListener("click", e => movetoright() )
+
+setInterval(() => {
+    movetoright()
+}, 3000);
+
+let operation = 0;
+let counter = 0;
+
+function movetoright(){
+    if(counter >= 2){
+        operation = 0;
+        counter= 0;
+        sliders.style.transform = `translate(-${operation}%)`; 
+        sliders.style.transition = "none";   
+        return;
+    }
+    counter++;
+    operation = operation + 33.3;
+    sliders.style.transform = `translate(-${operation}%)`;
+    sliders.style.transition = "all ease .6s";
+}
+function movetoleft(){
+    counter--;
+    if(counter <0){
+        operation = 33.3*2;
+        counter= 2;
+        sliders.style.transform = `translate(-${operation}%)`;    
+        sliders.style.transition = "none";  
+        return;
+    }
+    operation = operation - 33.3;
+    sliders.style.transform = `translate(-${operation}%)`;
+    sliders.style.transition = "all ease .6s";
+   
+}
